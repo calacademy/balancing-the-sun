77baadd3891c999ca83c797adc1e610968ae0566
Josh Ause <joshua.ause@gmail.com>
2019-03-26 14:34:26 -0700

    animation preloader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -8,11 +8,25 @@ class Loader extends Component {
     if (this.props.dataReady !== prevProps.dataReady) {
       setTimeout(function() {
         _this.props.handlerLoadComplete()
-      }, 10000)
+      }, 30000)
     }
   }
 
   render() {
+
+    let animFolate = []
+    let animVitamin = []
+
+    for (let i = 0; i < 450; i++) {
+      let fiver = ('0000' + i).slice(-5)
+      animFolate.push(
+        <img key={i} src={process.env.REACT_APP_ANIMATION_PATH + "animation/folate/BalancingTheSun-Folate_v05_" + fiver + ".png"} alt="" />
+      )
+      animVitamin.push(
+        <img key={i} src={process.env.REACT_APP_ANIMATION_PATH + "animation/vitamin/BalancingTheSun-VitaminD_v04_" + fiver + ".png"} alt="" />
+      )
+    }
+
     return(
       <div id="loader">
         <div id="asset-container">
@@ -21,6 +35,10 @@ class Loader extends Component {
             <div id="map-2" />
             <div id="map-3" />
             <div id="map-4" />
+          </div>
+          <div id="loader-animation-images">
+            {animFolate}
+            {animVitamin}
           </div>
           <p className="a">Test123!@#</p>
           <p className="b">Test123!@#</p>
